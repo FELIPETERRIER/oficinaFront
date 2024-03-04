ef6faf96b434273144f7345ca1e6f601761b1c7e
Felipe Bitencourt <felipeterrier@yahoo.com.br>
2024-03-04 14:37:17 -0300

    teste javascript

---
+++ oficina.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        id: 22,
+        cpf: '00000000000',
+        nome:'Fernando'
+        
+    },
+   
+    {
+        id: 23,
+        cpf: '00000000000',
+        nome:'Felipe'
+    }
+    ]
+    
+    console.log(Object.values(dados))
+    
